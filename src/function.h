c2959cdaa7d92447a3e904d9f2d2d2e28192b05b
n1kleb <n1kleb@localhost.localdomain>
2020-10-09 21:26:47 +0700

    Function declarations and type definitions for .c files

---
+++ src/function.h
@@ -0,0 +1,16 @@
+#ifndef FUNCTION_H
+#define FUNCTION_H
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define SIZE_BOARD 4
+
+void randomizer(int board[SIZE_BOARD][SIZE_BOARD]);
+void printBoard(int board[SIZE_BOARD][SIZE_BOARD]);
+void game(int board[SIZE_BOARD][SIZE_BOARD]);
+bool winChecking(int board[SIZE_BOARD][SIZE_BOARD]);
+
+#endif
